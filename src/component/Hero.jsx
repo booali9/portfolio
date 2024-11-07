6035fad79cddb3dfc571903298c20ba40477f0b5
Boo Ali Kazmi <155131719+booali9@users.noreply.github.com>
2024-11-07 08:06:56 -0500

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
 import { HERO_CONTENT } from '../consanant';
 
+
 export default function Hero() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
@@ -16,10 +17,10 @@ export default function Hero() {
     >
       <div className="flex-container">
         <motion.div
-          className="text"
-          initial={{ x: -100, opacity: 0 }}
-          animate={isInView ? { x: 0, opacity: 1 } : {}}
-          transition={{ duration: 0.5 }}
+           className="text"
+           initial={{ x: 100, opacity: 0 }}
+           animate={isInView ? { x: 0, opacity: 1 } : {}}
+           transition={{ duration: 1, delay: 1 }}
         >
           <h1 className='BACK'>Booali Kazmi</h1>
           <span className='dev'>Mern Stack Developer</span>
